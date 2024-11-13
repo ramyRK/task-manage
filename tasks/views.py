a902ad6ca38c2e2ad19bf01732123dabbb2b6dad
ramyRK <kheliliramy0@gmail.com>
2024-11-13 20:00:07 +0100

    views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .adapters import TaskConsoleAdapter
 
 # Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    formatted_tasks =[TaskConsoleAdapter(task).format_for_console() for task in tasks]
+    return render(request, 'tasks/task_list.html',{'tasks':formatted_tasks})
+
+
+def add_task(request):
+    if request.method=='POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        Task.objects.create(title=title,description=description)
+        return redirect(task_list)
+    return render(request,'tasks/add_task.html')
